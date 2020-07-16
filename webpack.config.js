1aa987ec5f96c207a1255504aa98414a5071e71a
Luke Stephenson <lukestephenson@live.co.uk>
2020-07-16 17:09:13 +0100

    Setup production webpack CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env) => {
     const isProduction = env === 'production'
-    
+
     return {
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "styles.css"
+            })
+        ],
         entry: './src/app.js',
         output: {
             path: path.join(__dirname, 'public'),
@@ -12,18 +18,28 @@ module.exports = (env) => {
         module: {
             rules: [{
                 loader: 'babel-loader',
-                test: /\.js$/ , 
+                test: /\.js$/,
                 exclude: /node_modules/
             }, {
-                test: /\.s?css$/ ,
+                test: /\.s?css$/,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
                 ]
             }]
         },
-        devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true
