d66c47dc006860ae59828f161e698619635035ba
Luke Stephenson <lukestephenson@live.co.uk>
2020-07-22 23:59:48 +0100

    Cleaned up ExpenseListItem by exporting localeData

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,29 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 import numeral from 'numeral'
+import localeData from '../locales/en-gb'
 
-numeral.register('locale', 'en-gb', {
-    delimiters: {
-        thousands: ',',
-        decimal: '.'
-    },
-    abbreviations: {
-        thousand: 'k',
-        million: 'm',
-        billion: 'b',
-        trillion: 't'
-    },
-    ordinal: function (number) {
-        var b = number % 10;
-        return (~~ (number % 100 / 10) === 1) ? 'th' :
-            (b === 1) ? 'st' :
-            (b === 2) ? 'nd' :
-            (b === 3) ? 'rd' : 'th';
-    },
-    currency: {
-        symbol: '£'
-    }
-})
+numeral.register('locale', 'en-gb', localeData)
 numeral.locale('en-gb')
 
 const ExpenseListItem = ({ description, amount, createdAt, id }) => (
